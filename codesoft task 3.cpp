b8b69ab4b5a58ab2d174bbe21166837b4a2cccd9
awasthiaman1706 <amanawasthi306@gmail.com>
2023-08-06 20:30:36 +0530

    codesoft task no 03

---
+++ codesoft task 3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include <fstream>
+
+#include <string>
+
+#include <sstream>
+
+int main() {
+  std::ifstream inputFile("weather.txt"); 
+
+  if (inputFile.is_open()) {
+    std::string line;
+    int wordCount = 0;
+
+    while (std::getline(inputFile, line)) {
+      std::stringstream ss(line);
+      std::string word;
+
+      while (ss >> word) {
+        wordCount++;
+      }
+    }
+
+    inputFile.close();
+
+    std::cout << "Number of words in the said file: " << wordCount << std::endl;
+  } else {
+    std::cout << "Failed to open the file." << std::endl;
+  }
+
+  return 0;
+}
